3e5043886f7541b05818288ccccdc41108b26aab
hcthcm290 <32518243+hcthcm290@users.noreply.github.com>
2021-08-05 13:37:12 +0700

    Finish tutorial 6

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -14,7 +14,14 @@ public class TankShooting : MonoBehaviour
     public float m_MaxLaunchForce = 30f; 
     public float m_MaxChargeTime = 0.75f;
 
-    /*
+    private enum GunState
+    {
+        Idle,
+        Charging,
+        Fire,
+    }
+
+    GunState m_Gunstate;
     private string m_FireButton;         
     private float m_CurrentLaunchForce;  
     private float m_ChargeSpeed;         
@@ -34,16 +41,66 @@ public class TankShooting : MonoBehaviour
 
         m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
     }
-    */
+    
 
     private void Update()
     {
         // Track the current state of the fire button and make decisions based on the current launch force.
+        if(m_Gunstate == GunState.Idle)
+        {
+            m_AimSlider.value = m_MinLaunchForce;
+            if (Input.GetButtonDown(m_FireButton))
+            {
+                m_Gunstate = GunState.Charging;
+                m_ShootingAudio.clip = m_ChargingClip;
+                m_ShootingAudio.loop = true;
+                m_ShootingAudio.Play();
+            }
+        }
+        else if(m_Gunstate == GunState.Charging)
+        {
+            m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+            m_AimSlider.value = m_CurrentLaunchForce;
+            
+
+            if(Input.GetButtonUp(m_FireButton))
+            {
+                m_Gunstate = GunState.Fire;
+            }
+
+            if(m_CurrentLaunchForce >= m_MaxLaunchForce)
+            {
+                m_CurrentLaunchForce = m_MaxLaunchForce;
+                m_Gunstate = GunState.Fire;
+                
+            }
+        }
+        else if(m_Gunstate == GunState.Fire)
+        {
+            Fire();
+
+            m_ShootingAudio.clip = m_FireClip;
+            m_ShootingAudio.loop = false;
+            m_ShootingAudio.Play();
+
+            m_CurrentLaunchForce = m_MinLaunchForce;
+            m_Gunstate = GunState.Idle;
+            m_AimSlider.value = m_MinLaunchForce;
+        }
+        else
+        {
+            Debug.LogError("Tank shooting goes into undefined state");
+        }
     }
 
 
     private void Fire()
     {
         // Instantiate and launch the shell.
+        Rigidbody shell = Instantiate(m_Shell);
+        shell.transform.position = m_FireTransform.position;
+        shell.transform.rotation = m_FireTransform.rotation;
+
+        shell.velocity = m_CurrentLaunchForce * shell.transform.forward;
     }
 }
